3175c8c738507ed9d47a76237cb89de2b674d7e1
samuelkoepsel1 <skoepsel@hotmail.com.br>
2021-08-24 01:11:24 -0300

    Fix.

--- src/Model/DadosConexao.java
+++ src/Model/DadosConexao.java
@@ -17,44 +17,20 @@ public class DadosConexao {
         return serverHost;
     }
 
-    public void setServerHost(String serverHost) {
-        this.serverHost = serverHost;
-    }
-
     public int getServerPort() {
         return serverPort;
-    }
-
-    public void setServerPort(int serverPort) {
-        this.serverPort = serverPort;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int getPrivatePort() {
         return privatePort;
     }
 
-    public void setPrivatePort(int privatePort) {
-        this.privatePort = privatePort;
-    }
-
     public int getPublicPort() {
         return publicPort;
-    }
-
-    public void setPublicPort(int publicPort) {
-        this.publicPort = publicPort;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
     }
 
     public String getHost() {
